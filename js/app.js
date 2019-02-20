bc72734483a2c39b9ed7241482e2eadf01d7ac48
Andrew Gilman <drewgilman@aol.com>
2019-02-19 19:37:01 -0600

    Almost finished with mobile layout

--- js/app.js
+++ js/app.js
@@ -1,3 +1,45 @@
-$(".photo-overlay").click(function(event) {
-  $(".photo-overlay").hide();
+//Remove all project overlays if click outside of #Portfolio
+  $('body').click(function(e){
+    if ($(e.target).parents().is('#portfolio')){
+      null
+    }else{
+      let overlay = $(".photo-overlay");
+      overlay.fadeOut();
+  };
+});
+
+//Show and Hide each project overlay when clicked
+ $('.photo-container').click(function(){
+   let overlay = $(".photo-overlay");
+   let thisOverlay = $(this).find(overlay);
+   if(thisOverlay.is(":visible")){
+     overlay.fadeOut();
+   } else {
+     $(overlay).fadeOut();
+     $(this).find(overlay).fadeIn();
+   }
+ });
+
+ //Rotate Hamburger when clicked and show/hide nav menu
+ $(".burgerButton").click(function(){
+   const burger = $(".burgerSVG");
+   const nav = $("nav");
+   if(burger.hasClass("rotate")){
+     burger.removeClass("rotate")
+     nav.slideUp();
+   } else {
+     burger.addClass("rotate")
+     nav.slideDown()
+   }
+ });
+
+ $('body').click(function(e){
+   if ($(e.target).parents().is('.burgerButton' || 'nav')){
+     null
+   }else{
+      const nav = $("nav");
+      const burger = $(".burgerSVG");
+      nav.slideUp();
+      burger.removeClass("rotate");
+ };
 });
